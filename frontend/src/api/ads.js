beee16379d867dd558009451a9f843d7b38685a2
Lakviru Perera <dinuwaperera123@gmail.com>
2026-05-15 19:06:11 +0530

    feat: add configuration file for A-Ads integration

--- frontend/src/api/ads.js
+++ frontend/src/api/ads.js
@@ -3,8 +3,8 @@
 
 export const AD_CONFIG = {
   HOME_TOP: "2437734", // 728x90 top ad
-  DETAILS_LEFT: null, 
-  DETAILS_RIGHT: null, 
+  DETAILS_LEFT: 2437739, 
+  DETAILS_RIGHT: 2437739, 
   DETAILS_MOBILE_TOP: null,
   DETAILS_MOBILE_BOTTOM: null,
 };
